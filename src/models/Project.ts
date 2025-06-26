c66f066ef9658c1b9e26297bde9634bd02678501
Arnold Rodriguez <arnolddavidrd@gmail.com>
2025-06-25 19:31:38 -0500

    Implement cascading delete for tasks and notes: add middleware to remove associated notes when a task or project is deleted

--- src/models/Project.ts
+++ src/models/Project.ts
@@ -1,6 +1,7 @@
 import mongoose, { Document, PopulatedDoc, Schema, Types } from "mongoose";
-import { ITask } from "./Task";
 import { IUser } from "./User";
+import Task, { ITask } from "./Task";
+import Note from "./Note";
 
 export interface IProject extends Document {
   projectName: string;
@@ -48,6 +49,29 @@ const ProjectSchema: Schema = new Schema(
   },
   { timestamps: true }
 );
+
+// Middleware to delele all tasks and notes associated with a project when the project is deleted
+ProjectSchema.pre("deleteOne", { document: true }, async function (next) {
+  const projectId = this._id as Types.ObjectId;
+
+  if (!projectId) return next();
+
+  try {
+    // Delete all notes associated with tasks in the project
+    const tasks = await Task.find({ project: projectId });
+    for (const task of tasks) {
+      await Note.deleteMany({ task: task._id });
+      console.log(`Deleted all notes associated with task ID: ${task._id}`);
+    }
+
+    // Delete all tasks associated with the project
+    await Task.deleteMany({ project: projectId });
+    console.log(`Deleted all tasks associated with project ID: ${projectId}`);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
 
 const Project = mongoose.model<IProject>("Project", ProjectSchema);
 export default Project;
--- src/models/Task.ts
+++ src/models/Task.ts
@@ -1,4 +1,5 @@
 import mongoose, { Schema, Document, Types } from "mongoose";
+import Note from "./Note";
 
 const taksStatus = {
   PENDING: "pending",
@@ -65,6 +66,40 @@ export const TaskSchema: Schema = new Schema(
   },
   { timestamps: true }
 );
+
+// Middleware to delete all notes associated with a task when the task is deleted
+
+// 1st approach: Using deleteOne with a query
+/* TaskSchema.pre("deleteOne", async function (next) {
+  try {
+    const taskId = this.getQuery()["_id"] as Types.ObjectId;
+    const notes = await mongoose.model("Note").find({ task: taskId });
+
+    for (const note of notes) {
+      console.log(`Deleting note with ID: ${note._id}`);
+      await mongoose.model("Note").deleteOne({ _id: note._id });
+    }
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+}); */
+
+// 2nd approach: Using deleteOne with document middleware
+TaskSchema.pre("deleteOne", { document: true }, async function (next) {
+  const taskId = this._id as Types.ObjectId;
+
+  if (!taskId) return next();
+
+  try {
+    await Note.deleteMany({ task: taskId });
+    console.log(`Deleted all notes associated with task ID: ${taskId}`);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
 
 const Task = mongoose.model<ITask>("Task", TaskSchema);
 export default Task;
